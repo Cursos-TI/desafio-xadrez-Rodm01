9977608b7fa11d4781e384c052c03a55deefcfb9
Rodolfo <rodolfoassuncao23@gmail.com>
2025-03-30 23:11:29 +0000

    incluindo identação e alteração na linha 9 e 10

--- xadrez.c
+++ xadrez.c
@@ -3,10 +3,10 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
+// por: Rodolfo Assunçao
 int main() {
  
-    int i = 1;
+    int i = 1;  // variavel de incremento
 
     do {
 
@@ -14,7 +14,7 @@ int main() {
         i++;
 
 
-    } while (i == 5);
+    } while (i <- 5);
 
  
  
